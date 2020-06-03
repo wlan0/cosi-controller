982510bda078e72fe00e2d0a7988b831810628c8
wlan0 <sidharthamn@gmail.com>
2020-06-02 17:18:21 -0700

    define bucket api object

---
+++ README.md
@@ -0,0 +1,3 @@
+COSI Controller
+---
+
--- api/v1alpha1/bucket_types.go
+++ api/v1alpha1/bucket_types.go
@@ -20,22 +20,57 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// Protcol is the underlying protocol used by the Object Storage bucket
+type Protocol string
+
+const (
+	// ProtocolS3V4 is the s3v4 API standard
+	ProtocolS3V4 Protocol = "s3v4"
+)
 
 // BucketSpec defines the desired state of Bucket
 type BucketSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Bucket. Edit Bucket_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Protocol is the underlying protocol used by the Object Storage bucket. eg. s3v4, gcs etc.
+	Protocol Protocol `json:"protocol"`
+
+	// BucketPrefix is the prefix path to the desired folder in a bucket
+	// +optional
+	BucketPrefix string `json:"bucketPrefix,omitempty"`
+
+	// BucketClassName is the name of the BucketClass where this Bucket belongs
+	// +optional
+	BucketClassName string `json:"bucketClassName,omitempty"`
+
+	// AccessSecretName is the name of the Secret object where the credentials for accessing this bucket is available
+	AccessSecretName string `json:"accessSecretName"`
 }
+
+// Phase denotes the status of this Bucket binding to a particular BucketContent
+type Phase string
+
+const (
+	// PhaseBound denotes that the Bucket is bound to a BucketContent
+	PhaseBound Phase = "bound"
+
+	// PhasePending denotes that the Bucket is waiting to be bound to a BucketContent
+	PhasePending Phase = "pending"
+)
 
 // BucketStatus defines the observed state of Bucket
 type BucketStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// BucketContentName is the name of the BucketContent resource created/bound by the COSI controller
+	// corresponding to the requested Bucket resource
+	// +optional
+	BucketContentName string `json:"bucketContentName,omitempty"`
+
+	// Phase denotes if the Bucket is bound to a BucketContent or not
+	Phase Phase `json:"phase"`
 }
 
 // +kubebuilder:object:root=true
